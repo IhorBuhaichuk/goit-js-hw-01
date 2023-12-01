f1c3990ee511fc9fa15fdc2ae6133d1157e1f0f4
Ihor Buhaichuk <ihorbugaichuk@gmail.com>
2023-12-01 16:37:50 +0200

    Done

---
+++ js/task-1.js
@@ -0,0 +1,5 @@
+function makeTransaction (quantity, pricePerDroid) {
+    return `You ordered ${quantity} droids worth ${pricePerDroid * quantity} credits!`
+}
+
+console.log(makeTransaction(5, 3000))
